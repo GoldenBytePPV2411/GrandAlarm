6be3c49794e9d8a3afde7c7fcd404d82299f37e2
VladislavBI <wbayrakvlad@gmail.com>
2016-03-14 00:45:44 +0200

    Добавлено главное окно

--- pills reminder/MainWindow.xaml.cs
+++ pills reminder/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +21,53 @@ namespace pills_reminder
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable drugsInfo { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            DrTableCreate();
+            var drugs = GetFullDrList();
+            TableToDGBinding(drugs);
+            DGInfo.ItemsSource = drugsInfo.AsDataView();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        void DrTableCreate()
         {
+            drugsInfo = new DataTable();
+            drugsInfo.Columns.Add("Название");
+            drugsInfo.Columns.Add("Время", typeof(TimeSpan));
+            drugsInfo.Columns.Add("От", typeof(DateTime));
+            drugsInfo.Columns.Add("До", typeof(DateTime));
+        }
 
+        IQueryable<WeekIntake> GetFullDrList()
+        {
+            PillsReminderEntities pl=new PillsReminderEntities();
+            var d = pl.WeekIntakes.Where(p => p.User.Id == App.curPnID);
+            return d;
+        }
+
+        void TableToDGBinding(IQueryable<WeekIntake> dr)
+        {
+            drugsInfo.Rows.Clear();
+            using(PillsReminderEntities plsr=new PillsReminderEntities())
+	        {
+             foreach (var item in dr)
+                {
+                    DataRow row = drugsInfo.NewRow();
+                    row[0] = item.Drug.DrugName;
+                    row[1] = item.Time;
+                    var tm=plsr.IntakeDurations.Where(p=>p.Id_person==App.curPnID&&p.Id_drug==item.Id_drug);
+                    foreach (var it in tm)
+                    {
+                      row[2] = it.DateFrom;
+                      row[3] = it.DateTo;  
+                    }
+                    drugsInfo.Rows.Add(row);
+                }
+
+	        }
+                
         }
     }
 }
